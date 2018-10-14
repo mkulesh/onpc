fb27d472ec6fd890ae48587d6975e8c2b282daa9
mkulesh <mikhail.kulesh@gmail.com>
2018-10-14 23:18:31 +0200

    Disabled debug logging

--- app/src/main/java/com/mkulesh/onpc/utils/Logging.java
+++ app/src/main/java/com/mkulesh/onpc/utils/Logging.java
@@ -13,12 +13,10 @@
 
 package com.mkulesh.onpc.utils;
 
-import android.util.Log;
-
 public final class Logging
 {
     public static void info(Object o, String text)
     {
-        Log.d("onpc", o.getClass().getSimpleName() + ": " + text);
+        //Log.d("onpc", o.getClass().getSimpleName() + ": " + text);
     }
 }
