4d784c5c991c4859f11e47d4c58082940ae96ee4
Mikhail Kulesh <mikhail.kulesh@gmail.com>
2019-11-09 18:02:18 +0100

    #91 iOS build for version 2.0: adapted build script

--- release.sh
+++ release.sh
@@ -10,3 +10,13 @@ flutter clean
 rm -f ios/Flutter/Generated.xcconfig
 rm -f ios/Flutter/flutter_export_environment.sh
 rm -rf android/.gradle
+
+rm -rf ios/.symlinks/
+rm -rf ios/Flutter/App.framework/
+rm -rf ios/Flutter/Flutter.framework/
+rm -rf ios/Flutter/Generated.xcconfig
+rm -rf ios/Flutter/flutter_export_environment.sh
+rm -rf ios/Podfile
+rm -rf ios/Podfile.lock
+rm -rf ios/Pods/
+rm -rf ios/ServiceDefinitions.json
