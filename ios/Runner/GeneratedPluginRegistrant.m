9a6bc8608b5018a0392e4159723d1900f1f3406b
Mikhail Kulesh <mikhail.kulesh@gmail.com>
2019-12-01 22:47:53 +0100

    #91 iOS build for version 2.0: Updated build environment

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -3,10 +3,30 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<fluttertoast/FluttertoastPlugin.h>)
 #import <fluttertoast/FluttertoastPlugin.h>
+#else
+@import fluttertoast;
+#endif
+
+#if __has_include(<package_info/PackageInfoPlugin.h>)
 #import <package_info/PackageInfoPlugin.h>
+#else
+@import package_info;
+#endif
+
+#if __has_include(<shared_preferences/SharedPreferencesPlugin.h>)
 #import <shared_preferences/SharedPreferencesPlugin.h>
+#else
+@import shared_preferences;
+#endif
+
+#if __has_include(<url_launcher/UrlLauncherPlugin.h>)
 #import <url_launcher/UrlLauncherPlugin.h>
+#else
+@import url_launcher;
+#endif
 
 @implementation GeneratedPluginRegistrant
 
--- release.sh
+++ release.sh
@@ -16,8 +16,11 @@ rm -rf ios/Flutter/App.framework/
 rm -rf ios/Flutter/Flutter.framework/
 rm -rf ios/Flutter/Generated.xcconfig
 rm -rf ios/Flutter/flutter_export_environment.sh
+rm -rf ios/Flutter/Flutter.podspec
 rm -rf ios/Podfile
 rm -rf ios/Frameworks
 rm -rf ios/Podfile.lock
 rm -rf ios/Pods/
 rm -rf ios/ServiceDefinitions.json
+rm -rf ios/Runner.xcworkspace/xcshareddata/
+rm -rf ios/Runner.xcworkspace/xcuserdata/
