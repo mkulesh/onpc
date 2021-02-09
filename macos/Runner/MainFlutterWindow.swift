b0fb177b07cd2827e9bcacb42127928a51d7baa3
Mikhail Kulesh <mikhail.kulesh@gmail.com>
2021-02-09 22:57:17 +0100

    #209 Prepare macOS build: set default app size

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -4,7 +4,7 @@ import FlutterMacOS
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController.init()
-    let windowFrame = self.frame
+    let windowFrame = NSRect(x:100, y:100, width: 350, height: 720)
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
 
