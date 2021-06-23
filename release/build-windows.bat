b39d2a5f67875ab0b61062b35e268ad12e599bff
Mikhail Kulesh <mikhail.kulesh@gmail.com>
2021-06-23 23:53:37 +0200

    Improved Windows build

--- release/build-windows.bat
+++ release/build-windows.bat
@@ -4,11 +4,17 @@ set /p VER=<VERSION.txt
 echo Building MusicControl-v%VER%-windows-x86_64
 
 :: The Window build can be currently done on master channel only
+call del *.msix
 call flutter clean
 call flutter channel stable
 call flutter doctor
 call flutter build windows --release
 
+:: Create msix installer
+call flutter pub run msix:create
+call move ..\build\windows\runner\Release\onpc.msix MusicControl-v%VER%-windows-x86_64.msix
+
+:: Create native TAR
 call copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.28.29325\x64\Microsoft.VC142.CRT\msvcp140.dll" "..\build\windows\runner\Release"
 call copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.28.29325\x64\Microsoft.VC142.CRT\vcruntime140.dll" "..\build\windows\runner\Release"
 call copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.28.29325\x64\Microsoft.VC142.CRT\vcruntime140_1.dll" "..\build\windows\runner\Release"
