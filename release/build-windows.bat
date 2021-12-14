ab70379083dd91ef284f62d0792a72e99b29f908
Mikhail Kulesh <mikhail.kulesh@gmail.com>
2021-12-14 21:53:54 +0100

    #243 Prepare Windows build: applied Windows Store settings

--- release/build-windows.bat
+++ release/build-windows.bat
@@ -2,6 +2,7 @@ echo off
 
 set /p VER=<VERSION.txt
 echo Build Windows app MusicControl-v%VER%-windows-x86_64
+echo Windows Store: pass --store flag as argument
 
 :: Prepare Yaml file
 call copy ..\pubspec.yaml_desktop ..\pubspec.yaml
@@ -13,18 +14,18 @@ call copy ..\lib\utils\CompatUtils.dart.desktop ..\lib\utils\CompatUtils.dart
 :: Flutter version 2.2.2, Dart version 2.13.3
 call del *.msix
 call flutter clean
-call flutter channel stable
 call flutter doctor
 call flutter build windows --release
 
 :: Create msix installer
-call flutter pub run msix:create
+echo Windows Store flag: %1
+call flutter pub run msix:create %1
 call move ..\build\windows\runner\Release\onpc.msix MusicControl-v%VER%-windows-x86_64.msix
 
 :: Create native TAR
-call copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.28.29325\x64\Microsoft.VC142.CRT\msvcp140.dll" "..\build\windows\runner\Release"
-call copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.28.29325\x64\Microsoft.VC142.CRT\vcruntime140.dll" "..\build\windows\runner\Release"
-call copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.28.29325\x64\Microsoft.VC142.CRT\vcruntime140_1.dll" "..\build\windows\runner\Release"
-call rename ..\build\windows\runner\Release\music_control.exe "Music Control.exe"
-call move ..\build\windows\runner\Release MusicControl-v%VER%-windows-x86_64
-call tar -acf MusicControl-v%VER%-windows-x86_64.zip MusicControl-v%VER%-windows-x86_64
+:: call copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.28.29325\x64\Microsoft.VC142.CRT\msvcp140.dll" "..\build\windows\runner\Release"
+:: call copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.28.29325\x64\Microsoft.VC142.CRT\vcruntime140.dll" "..\build\windows\runner\Release"
+:: call copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.28.29325\x64\Microsoft.VC142.CRT\vcruntime140_1.dll" "..\build\windows\runner\Release"
+:: call rename ..\build\windows\runner\Release\music_control.exe "Music Control.exe"
+:: call move ..\build\windows\runner\Release MusicControl-v%VER%-windows-x86_64
+:: call tar -acf MusicControl-v%VER%-windows-x86_64.zip MusicControl-v%VER%-windows-x86_64
