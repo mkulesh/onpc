487e1b2b37de75942c24b37f615615f6265d7d09
Mikhail Kulesh <mikhail.kulesh@gmail.com>
2022-11-14 22:40:00 +0100

    Updated macOS

--- release/build-ios.sh
+++ release/build-ios.sh
@@ -8,7 +8,7 @@ ln -s pubspec.yaml_mobile ../pubspec.yaml
 
 # Build with: Flutter version 2.10.1, Dart version 2.15.1
 flutter clean
-cd /Volumes/ExtWork/home/family/work/android/flutter
+cd /Volumes/work/android/flutter
 git checkout 2.10.1
 cd -
 flutter doctor -v
