82464c0fba79b5a9eefce1e17132dfce6b3fc87a
Mikhail Kulesh <mikhail.kulesh@gmail.com>
2022-11-24 06:45:57 -0800

    Fixed minor bugs

--- lib/config/KeyboardShortcuts.dart
+++ lib/config/KeyboardShortcuts.dart
@@ -63,8 +63,8 @@ class _KeyboardShortcutsState extends State<KeyboardShortcuts>
         rows.add(_buildRow(td, Strings.master_volume_down,   "ks_volume_down"));
         rows.add(_buildRow(td, Strings.audio_muting_toggle,  "ks_volume_mute"));
         rows.add(_buildRow(td, Strings.cmd_description_trdn, "ks_volume_trdn"));
-        rows.add(_buildRow(td, Strings.cmd_description_play, "ks_volume_play"));
-        rows.add(_buildRow(td, Strings.cmd_description_pause,"ks_volume_stop"));
+        rows.add(_buildRow(td, Strings.cmd_description_p_p,  "ks_volume_play"));
+        rows.add(_buildRow(td, Strings.cmd_description_stop, "ks_volume_stop"));
         rows.add(_buildRow(td, Strings.cmd_description_trup, "ks_volume_trup"));
 
         final Map<int, TableColumnWidth> columnWidths = Map();
