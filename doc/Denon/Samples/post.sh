2f04a0e8a4ad5b113f6410b93b41f21c63ca0723
Mikhail Kulesh <mikhail.kulesh@gmail.com>
2023-04-04 17:54:10 +0200

    #275 Add Denon communication protocol: added documentation

---
+++ doc/Denon/Samples/post.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo ----- GetAllZonePowerStatus:
+curl -X POST -H "Content-Type: text/xml" \
+    -d '{"body": "<?xml version="1.0" encoding="utf-8"?><tx><cmd id="1">GetAllZonePowerStatus</cmd><cmd id="1">GetZoneName</cmd><cmd id="1">GetAllZoneVolume</cmd><cmd id="1">GetAllZoneSource</cmd><cmd id="1">GetAllZoneMuteStatus</cmd></tx>"}' \
+    http://192.168.1.82:8080/goform/AppCommand.xml
+
+echo ----- GetSourceRename:
+curl -X POST -H "Content-Type: text/xml" \
+    -d '{"body": "<?xml version="1.0" encoding="utf-8"?><tx><cmd id="3"><name>GetSourceRename</name><list/></cmd></tx>"}' \
+    http://192.168.1.82:8080/goform/AppCommand0300.xml
+
+echo ----- GetSoundMode:
+curl -X POST -H "Content-Type: text/xml" \
+    -d '{"body": "<?xml version="1.0" encoding="utf-8"?><tx><cmd id="3"><name>GetSoundMode</name><list><param name="movie"></param><param name="music"></param><param name="game"></param><param name="pure"></param></list></cmd></tx>"}' \
+    http://192.168.1.82:8080/goform/AppCommand0300.xml
+
+echo ----- GetSoundMode:
+curl -X POST -H "Content-Type: text/xml" \
+    -d '{"body": "<?xml version="1.0" encoding="utf-8"?><tx><cmd id="3"><name>GetSoundMode</name><list><param name="movie"></param><param name="music"></param><param name="game"></param><param name="pure"></param></list></cmd></tx>"}' \
+    http://192.168.1.82:8080/goform/AppCommand0300.xml
+
+
+echo ----- Deviceinfo:
+curl http://192.168.1.82:8080/goform/Deviceinfo.xml
