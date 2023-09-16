88c298fd54c14da746017226bdd551799df39848
Mikhail Kulesh <mikhail.kulesh@gmail.com>
2023-09-16 22:27:02 +0200

    #281 Increase Flutter version: updated XCode configuration

--- release/build-macos.sh
+++ release/build-macos.sh
@@ -14,10 +14,10 @@ echo Build macOS app...
 rm -f ../pubspec.yaml
 ln -s pubspec.yaml_desktop ../pubspec.yaml
 
-# Build with: Flutter version 2.10.1, Dart version 2.15.1
+# Build with: Flutter version 3.7.10, Dart version 2.19.6
 flutter clean
 cd /Volumes/work/android/flutter
-git checkout 2.10.1
+git checkout 3.7.10
 cd -
 flutter doctor -v
 
